corrected loss_term calculation, removed some print statements, and set ylim for plotting

main.py
@@ -25,12 +25,14 @@ if __name__ == "__main__":
     Calculate the sampled FOR
     '''
     # Create total active and reactive load linspaces
-    n_points = 11
+
+    n_points = 21
     max_active_power = sum([n.p_max for n in graph.nodes])
     max_reactive_power = sum([n.q_max for n in graph.nodes])
 
     p_totals = np.linspace(0, max_active_power, n_points)
     q_totals = np.linspace(0, max_reactive_power, n_points)
+    # '''
 
     # Allocate result lists
     Ps_transformer = []
@@ -74,6 +76,7 @@ if __name__ == "__main__":
             print(f'Progress sampling: {round(100 * (count + 1)/n_points ** 2, 2)}%')
             count += 1
 
+    # '''
     '''
     Calculate voltage cuts
     
@@ -82,17 +85,17 @@ if __name__ == "__main__":
     iteration.
     
     Example:
-        loss_parameter = 0: losses are calculated based on the branch flows at the beginning of the iteration
-        loss_parameter = 1: losses are calculated based on the branch flows at the end of the iteration
+        loss_parameter = 0: losses are calculated based on the branch flows at the end beginning of the iteration
+        loss_parameter = 1: losses are calculated based on the branch flows at the beginning of the iteration
         loss_parameter = 0.5: branch flows are the average of the flows at the beginning/end of the iteration
     '''
 
-    '''
+    # '''
     slopes, intercepts = cut_calculator.get_cut_slopes_intercepts(v_properties, include_losses=False)
     slopes_losses, intercepts_losses = cut_calculator.get_cut_slopes_intercepts(v_properties,
-                                                                               include_losses=True,
-                                                                               loss_parameter=0.5)
-    '''
+                                                                                include_losses=True,
+                                                                                loss_parameter=0.0)
+    # '''
 
     '''
     Plotting
@@ -102,7 +105,7 @@ if __name__ == "__main__":
     plt.scatter(Ps_transformer_no_loss, Qs_transformer_no_loss, color='r', label='Linear Distflow')
 
     # Cuts
-    '''
+    # '''
     cut_ids = slopes.keys()
     cut_ids_to_show = cut_ids
     cut_ids_losses_to_show = cut_ids
@@ -118,9 +121,10 @@ if __name__ == "__main__":
 
         q_values = intercept + slope * 1000 * p_totals  # p_totals is in kW
         q_values_losses = intercept_loss + slope * 1000 * p_totals
-        plt.plot(p_totals, q_values/1000.0, 'r', label=str(cut_id))
+        # plt.plot(p_totals, q_values/1000.0, 'r', label=str(cut_id))
         plt.plot(p_totals, q_values_losses / 1000.0, 'b', label=str(cut_id))
 
-    plt.legend()
-    '''
+    # plt.legend()
+    # '''
+    plt.ylim(0, 1.1 * max_reactive_power)
     plt.show()